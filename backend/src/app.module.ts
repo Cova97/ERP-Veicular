d6a6523666ef47f663cb1970fa67174aec961103
Aldo <60949501+Cova97@users.noreply.github.com>
2026-03-18 10:56:45 -0600

    Update
    Se renombro la carpeta de modules por models

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { MarcaModule } from './modules/marca/marca.module';
-import { ModeloModule } from './modules/modelo/modelo.module';
-import { SubmarcaModule } from './modules/submarca/submarca.module';
-import { PropietarioModule } from './modules/propietario/propietario.module';
-import { VehiculoModule } from './modules/vehiculo/vehiculo.module';
-import { ServicioAceiteModule } from './modules/servicio-aceite/servicio-aceite.module';
-import { VerificacionModule } from './modules/verificacion/verificacion.module';
-import { TenenciaModule } from './modules/tenencia/tenencia.module';
-import { ServicioLlantasModule } from './modules/servicio-llantas/servicio-llantas.module';
-import { ServicioAmortiguadorModule } from './modules/servicio-amortiguador/servicio-amortiguador.module';
-import { ServicioFrenoModule } from './modules/servicio-freno/servicio-freno.module';
+import { MarcaModule } from './models/marca/marca.module';
+import { ModeloModule } from './models/modelo/modelo.module';
+import { SubmarcaModule } from './models/submarca/submarca.module';
+import { PropietarioModule } from './models/propietario/propietario.module';
+import { VehiculoModule } from './models/vehiculo/vehiculo.module';
+import { ServicioAceiteModule } from './models/servicio-aceite/servicio-aceite.module';
+import { VerificacionModule } from './models/verificacion/verificacion.module';
+import { TenenciaModule } from './models/tenencia/tenencia.module';
+import { ServicioLlantasModule } from './models/servicio-llantas/servicio-llantas.module';
+import { ServicioAmortiguadorModule } from './models/servicio-amortiguador/servicio-amortiguador.module';
+import { ServicioFrenoModule } from './models/servicio-freno/servicio-freno.module';
 
 @Module({
   imports: [MarcaModule, ModeloModule, SubmarcaModule, PropietarioModule, VehiculoModule, ServicioAceiteModule, VerificacionModule, TenenciaModule, ServicioLlantasModule, ServicioAmortiguadorModule, ServicioFrenoModule],

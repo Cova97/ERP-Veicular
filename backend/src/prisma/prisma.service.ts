35d38eda86e3bc04e46f6a6ad87b025a696ded7c
Aldo <60949501+Cova97@users.noreply.github.com>
2026-04-02 19:18:08 -0600

    Fix
    se corrigieron errores del prisma que no estaba jalando bien el client

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaClient } from '@prisma/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import 'dotenv/config';
 
